a59a1cf95ad708067edde1d322ed6c98cfbe3dce
Natalka <ntyszkiewicz@gmail.com>
2015-07-24 14:32:26 +0200

    add raketask to start modifier & byebug

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 ruby '2.2.1'
 
 gem 'byebug'
+gem 'rake'
 gem 'rspec'
--- modifier.rb
+++ modifier.rb
@@ -1,22 +1,7 @@
 require File.expand_path('lib/combiner',File.dirname(__FILE__))
 require 'csv'
 require 'date'
-
-def latest(name)
-  files = Dir["#{ ENV["HOME"] }/workspace/*#{name}*.txt"]
-
-  files.sort_by! do |file|
-    last_date = /\d+-\d+-\d+_[[:alpha:]]+\.txt$/.match file
-    last_date = last_date.to_s.match /\d+-\d+-\d+/
-
-    date = DateTime.parse(last_date.to_s)
-    date
-  end
-
-  throw RuntimeError if files.empty?
-
-  files.last
-end
+require 'byebug'
 
 class String
   def from_german_to_f
@@ -177,11 +162,3 @@ class Modifier
     return output
   end
 end
-
-modified = input = latest('project_2012-07-27_2012-10-10_performancedata')
-modification_factor = 1
-cancellaction_factor = 0.4
-modifier = Modifier.new(modification_factor, cancellaction_factor)
-modifier.modify(modified, input)
-
-puts "DONE modifying"
